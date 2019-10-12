e0ef509ab1fb4e0e74212e53aadbd2f202416252
ShimeWataru <wawawatataru@gmail.com>
2019-10-12 19:57:16 +0900

    C埋めの続き

--- abc125/c.py
+++ abc125/c.py
@@ -10,20 +10,27 @@ logging.basicConfig(level=logging.DEBUG)
 def resolve():
     n = int(input())
     l = list(map(int, input().split()))
-    sum = 0
+    ans = 0
+    front = [0] * len(l)
+    back = [0] * len(l)
+    front[0] = l[0]
+    back[0] = l[-1]
     if len(l) == 2:
-        print(max(l[0], l[1]))
-        exit()
+        ans = max(l[0], l[1])
     else:
-        for i in range(n):
-            ll = copy.deepcopy(l)
-            ll.pop(i)
-            ans = ll[0]
-            for j in range(1, len(ll)):
-                ans = fractions.gcd(ans, ll[j])
-                if sum < ans:
-                    sum = ans
-    print(sum)
+        for i in range(len(l) - 1):
+            front[i + 1] = fractions.gcd(front[i], l[i+1])
+            back[i + 1] = fractions.gcd(back[i], l[-i - 2])
+        for i in range(len(l)):
+            if i == 0:
+                ans_i = back[-len(l) + 1]
+            elif i == len(l) - 1:
+                ans_i = front[len(l) - 1]
+            else:
+                ans_i = fractions.gcd(front[i - 1], back[len(l) - (i + 2)])
+            if ans < ans_i:
+                ans = ans_i
+    print(ans)
 
 
 class TestClass(unittest.TestCase):
@@ -38,9 +45,9 @@ class TestClass(unittest.TestCase):
 
     def test_input_1(self):
         print("test_input_1")
-        input = """3
-7 6 8"""
-        output = """2"""
+        input = """4
+7 6 8 9"""
+        output = """1"""
         self.assertIO(input, output)
 
     def test_input_2(self):
