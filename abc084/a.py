e4a0e1f2e992827c211c22e323189505d80ef9da
ShimeWataru <wawawatataru@gmail.com>
2020-02-11 08:45:32 +0900

    abc084の練習

---
+++ abc084/a.py
@@ -0,0 +1,36 @@
+import sys
+from io import StringIO
+import unittest
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+def resolve():
+    print(24 + 24 - int(input()))
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_input_1(self):
+        print("test_input_1")
+        input = """21"""
+        output = """27"""
+        self.assertIO(input, output)
+
+    def test_input_2(self):
+        print("test_input_2")
+        input = """12"""
+        output = """36"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
