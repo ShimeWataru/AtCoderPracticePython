2d60f9012de3f3894063c434798d97b37065e5c4
ShimeWataru <wawawatataru@gmail.com>
2020-08-10 11:30:06 +0900

    abc060 c

---
+++ abc060/c.py
@@ -0,0 +1,60 @@
+import sys
+from io import StringIO
+import unittest
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def resolve():
+    n, t = map(int, input().split())
+    l = list(map(int, input().split()))
+    old = 0
+    ans = t
+    for i in range(1,n):
+        ans += l[i] + t - max(old + t,l[i])
+        old = l[i]
+    print(ans)
+
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_input_1(self):
+        print("test_input_1")
+        input = """2 4
+0 3"""
+        output = """7"""
+        self.assertIO(input, output)
+    def test_input_2(self):
+        print("test_input_2")
+        input = """2 4
+0 5"""
+        output = """8"""
+        self.assertIO(input, output)
+    def test_input_3(self):
+        print("test_input_3")
+        input = """4 1000000000
+0 1000 1000000 1000000000"""
+        output = """2000000000"""
+        self.assertIO(input, output)
+    def test_input_4(self):
+        print("test_input_4")
+        input = """1 1
+0"""
+        output = """1"""
+        self.assertIO(input, output)
+    def test_input_5(self):
+        print("test_input_5")
+        input = """9 10
+0 3 5 7 100 110 200 300 311"""
+        output = """67"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
