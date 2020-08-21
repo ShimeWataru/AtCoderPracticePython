a464ca7b11024dd22e6eb9fd208355dfa0abd7b1
ShimeWataru <wawawatataru@gmail.com>
2020-08-22 08:14:07 +0900

    tenka1-2019-beginner ab

---
+++ tenka1-2019-beginner/a.py
@@ -0,0 +1,47 @@
+import sys
+from io import StringIO
+import unittest
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def resolve():
+    a, b, c = map(int, input().split())
+    if a < c < b:
+        print("Yes")
+    elif a > c > b:
+        print("Yes")
+    else:
+        print("No")
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_input_1(self):
+        print("test_input_1")
+        input = """3 8 5"""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_input_2(self):
+        print("test_input_2")
+        input = """7 3 1"""
+        output = """No"""
+        self.assertIO(input, output)
+    def test_input_3(self):
+        print("test_input_3")
+        input = """10 2 4"""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_input_4(self):
+        print("test_input_4")
+        input = """31 41 59"""
+        output = """No"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
